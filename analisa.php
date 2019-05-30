c7afa35ae423357d234a0f7849b5002db8ddb467
Andhika Prima Jaya <andhika644@gmail.com>
2019-05-30 10:18:58 +0800

    Update analisa.php

--- analisa.php
+++ analisa.php
@@ -1,5 +1,4 @@
 <?php
-
 if (isset($_POST['submit'])) {
 	if (isset($_POST['url'])) {
 		$url = $_POST['url'];
@@ -9,97 +8,109 @@ if (isset($_POST['submit'])) {
 } else {
 	header("Location: index.php");
 }
-
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Computer Vision Azure!</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-</head>
-<body>
-	<script type="text/javascript">
-		$(document).ready(function () {
-        // **********************************************
-        // *** Update or verify the following values. ***
-        // **********************************************
-
-        // Replace Subscription Key with your valid subscription key.
-        var subscriptionKey = "Subscription Key";
-
-        // You must use the same Azure region in your REST API method as you used to
-        // get your subscription keys. For example, if you got your subscription keys
-        // from the West US region, replace "westcentralus" in the URL
-        // below with "westus".
-        //
-        // Free trial subscription keys are generated in the "westus" region.
-        // If you use a free trial subscription key, you shouldn't need to change
-        // this region.
-        var uriBase =
-        "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
-
-        // Request parameters.
-        var params = {
-        	"visualFeatures": "Categories,Description,Color",
-        	"details": "",
-        	"language": "en",
-        };
-
-        // Display the image.
-        var sourceImageUrl = "<?php echo $url ?>";
-        document.querySelector("#sourceImage").src = sourceImageUrl;
-
-        // Make the REST API call.
-        $.ajax({
-        	url: uriBase + "?" + $.param(params),
-
-            // Request headers.
-            beforeSend: function(xhrObj){
-            	xhrObj.setRequestHeader("Content-Type","application/json");
-            	xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-            },
-
-            type: "POST",
-
-            // Request body.
-            data: '{"url": ' + '"' + sourceImageUrl + '"}',
-        })
-
-        .done(function(data) {
-            // Show formatted JSON on webpage.
-            $("#responseTextArea").val(JSON.stringify(data, null, 2));
-            // console.log(data);
-            // var json = $.parseJSON(data);
-            $("#description").text(data.description.captions[0].text);
-        })
-
-        .fail(function(jqXHR, textStatus, errorThrown) {
-            // Display error message.
-            var errorString = (errorThrown === "") ? "Error. " :
-            errorThrown + " (" + jqXHR.status + "): ";
-            errorString += (jqXHR.responseText === "") ? "" :
-            jQuery.parseJSON(jqXHR.responseText).message;
-            alert(errorString);
-        });
-    });
-</script>
-
-<h1>Analisa Gambar:</h1>
-<div id="wrapper" style="width:1020px; display:table;">
-	<div id="jsonOutput" style="width:600px; display:table-cell;">
-		Response:
-		<br><br>
-		<textarea id="responseTextArea" class="UIInput"
-		style="width:580px; height:400px;" readonly=""></textarea>
-	</div>
-	<div id="imageDiv" style="width:420px; display:table-cell;">
-		Sumber Gambar:
-		<br><br>
-		<img id="sourceImage" width="400" />
-		<br>
-		<h3 id="description">Memuat Deskripsi Gambar. . .</h3>
-	</div>
-</div>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="Submission 2 MACD">
+		<meta name="author" content="Andhika Prima Jaya">
+		<title>Computer Vision Azure!</title>
+		<!-- Icon -->
+		<link rel="icon" href="https://sqlvafkp24pamflmsg.blob.core.windows.net/blockblobshxjgdp/apj.ico">
+		<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+		<!-- Bootstrap core CSS -->
+		<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="starter-template.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	</head>
+	
+	<body>
+				
+			<main role="main" class="container">
+				<div class="starter-template">
+					<br><br><br>
+					<h1>Hasil Analisa Gambar</h1>
+					<span class="border-top my-3"></span>
+				</div>
+				
+				<script type="text/javascript">
+					$(document).ready(function () {
+					// **********************************************
+					// *** Update or verify the following values. ***
+					// **********************************************
+	
+						// Replace [Key] with your valid subscription key.
+						var subscriptionKey = "[Key]";
+						
+						// You must use the same Azure region in your REST API method as you used to
+						// get your subscription keys. For example, if you got your subscription keys
+						// from the West US region, replace "westcentralus" in the URL
+						// below with "westus".
+						//
+						// Free trial subscription keys are generated in the "westus" region.
+						// If you use a free trial subscription key, you shouldn't need to change
+						// this region.
+						var uriBase = "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+						
+						// Request parameters.
+						var params = {
+							"visualFeatures": "Categories,Description,Color",
+							"details": "",
+							"language": "en",
+						};
+						
+						// Display the image.
+						var sourceImageUrl = "<?php echo $url ?>";
+						document.querySelector("#sourceImage").src = sourceImageUrl;
+						
+						// Make the REST API call.
+						$.ajax({
+							url: uriBase + "?" + $.param(params),
+							
+							// Request headers.
+							beforeSend: function(xhrObj){
+								xhrObj.setRequestHeader("Content-Type","application/json");
+								xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+							},
+							type: "POST",
+							
+							// Request body.
+							data: '{"url": ' + '"' + sourceImageUrl + '"}',
+						})
+							.done(function(data) {
+							
+							// Show formatted JSON on webpage.
+							$("#responseTextArea").val(JSON.stringify(data, null, 2));
+							$("#description").text(data.description.captions[0].text);
+						})
+							.fail(function(jqXHR, textStatus, errorThrown) {
+							
+							// Display error message.
+							var errorString = (errorThrown === "") ? "Error. " :
+							errorThrown + " (" + jqXHR.status + "): ";
+							errorString += (jqXHR.responseText === "") ? "" :
+							jQuery.parseJSON(jqXHR.responseText).message;
+							alert(errorString);
+						});
+					});
+				</script>
+				<br>
+				
+				<div id="wrapper" style="width:1020px; display:table;">
+					<div id="jsonOutput" style="width:600px; display:table-cell;">
+						<b>Response:</b><br><br>
+						<textarea id="responseTextArea" class="UIInput"
+							  style="width:580px; height:400px;" readonly></textarea>
+					</div>
+					<div id="imageDiv" style="width:420px; display:table-cell;">
+						<b>Sumber Gambar:</b><br><br>
+						<img id="sourceImage" width="400" /><br>
+						<h3 id="description">...</h3>
+					</div>
+				</div>
+	</body>
 </html>
