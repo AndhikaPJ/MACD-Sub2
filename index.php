831230459764f6b7014609818052b50651e7b48c
Andhika Prima Jaya <andhika644@gmail.com>
2019-05-30 10:16:41 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
 
 require_once 'vendor/autoload.php';
-require_once "./random_string.php";
+require_once "random_string.php";
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+//Ganti [Account] dan [Key] dengan Data yang ada di Storage/Access Key
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=[Account];AccountKey=[Key];";
 
-//Ganti [Name] & [Key] dengan nama Account Name & Key di Access Key Storage
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=[Name];AccountKey=[Key];";
-
-//Ganti nameblob dengan Container Name yang anda buat di Storage/Blob
-$containerName = "nameblob";
+//Ganti BlobName dengan Nama Container di Storage/Blob
+$containerName = "BlobName";
 
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
@@ -21,7 +20,7 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
 if (isset($_POST['submit'])) {
 	$fileToUpload = strtolower($_FILES["fileToUpload"]["name"]);
 	$content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-	// echo fread($content, filesize($fileToUpload));
+	echo fread($content, filesize($fileToUpload));
 
 	$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 	header("Location: index.php");
@@ -37,8 +36,12 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Submission 2 MACD">
+	<meta name="author" content="">
 	<title>Computer Vision Azure!</title>
-	<link rel="icon" href="https://sqlvafkp24pamflmsg.blob.core.windows.net/blockblobshxjgdp/apj.ico">
+	<link rel="icon" href="https://sqlvafkp24pamflmsg.blob.core.windows.net/blockblobshxjgdp/search.ico">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
@@ -89,5 +92,11 @@ $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
 		</table>
 
 	</div>
+	
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+			<script src="https://getbootstrap.com/docs/4.0/assets/js/vendor/popper.min.js"></script>
+			<script src="https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js"></script>
+	
 </body>
 </html>
